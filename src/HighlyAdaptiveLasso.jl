91316ee06cabd5ab5eb89d89ed2c7fcd1c5593c3
Olivier Labayle <olabayle@gmail.com>
2021-05-20 18:17:11 +0100

    start to work on HALRegressor

--- src/HighlyAdaptiveLasso.jl
+++ src/HighlyAdaptiveLasso.jl
@@ -5,16 +5,28 @@ using MLJModelInterface: @mlj_model
 using RCall
 
 
-@mlj_model mutable struct HAL <: MLJModelInterface.Deterministic
+@mlj_model mutable struct HALRegressor <: MLJModelInterface.Deterministic
 end
 
 
-function MLJModelInterface.fit(m::HAL, verbose::Int, X, y)
+function MLJModelInterface.fit(m::HALRegressor, verbose::Int, X, y)
 
-    fitresult = R"fit_hal(X = x, Y = y)"
+    fitresult = R"fit_hal(X = $X, Y = $y)"
     cache = nothing
     report = NamedTuple()
     return (fitresult, cache, report)
 end
+
+
+function MLJModelInterface.fitted_params(m::HALRegressor, fitresult)
+    return rcopy(fitresult)
+end
+
+
+function MLJModelInterface.predict(m::HALRegressor, fitresult, Xnew)
+    ypred = R"predict($fitresult, new_data = $Xnew)"
+    return rcopy(ypred)
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,38 @@
-using HighlyAdaptiveLasso
+using HighlyAdaptiveLasso: HALRegressor
 using Test
-using RCall
+using MLJBase
 
 @testset "HighlyAdaptiveLasso.jl" begin
+    # Those are sanity checks as the original package is already well tested
+    n = 100
+    p = 3
+    X = randn(n, p)
+    y = X[:, 1] .* 3sin.(X[:, 2]) + randn(n)
 
-    @test R"library(hal9001)" isa RObject
+    model = HALRegressor()
+    mach = machine(model, X, y)
+    fit!(mach)
+    fp = fitted_params(mach)
+    # Checking keys associated with fitted params
+    # From the warning, some are lost, but it's only for end user inspection
+    @test Set(keys(fp)) == Set([:copy_map,
+                                :x_basis,
+                                :lambda_star,
+                                :col_lists,
+                                :glmnet_lasso,
+                                :call,
+                                :times,
+                                :hal_lasso,
+                                :coefs,
+                                :basis_list,
+                                :family,
+                                :unpenalized_covariates,
+                                :reduce_basis])
+
+    # Checking basic prediction is better than the mean
+    ypred = predict(mach, X)
+    mse = mean((y - ypred).^2)
+    avg_mse = mean((y .- mean(y)).^2)
+    @test mse < avg_mse
     
 end
