50030d0f56924036648b7202181de15a87ba8357
Olivier Labayle <48188914+olivierlabayle@users.noreply.github.com>
2021-11-02 11:45:57 +0000

    Update deps (#4)
    * update to new version
    
    * add linux support in CI
    
    * try again
    
    * update version
    
    * update dataframes version
    
    * dataframes posing pb
    
    * try another build
    
    * try debug
    
    * remove ubuntu as even RCall does not build on it
    
    * only macos

--- deps/build.jl
+++ deps/build.jl
@@ -1,3 +1,7 @@
 using RCall
 
-R"install.packages('hal9001', repos='https://cloud.r-project.org/')"
+R"""
+install.packages('devtools', repos='http://cran.us.r-project.org')
+require(devtools)
+install_version("hal9001", version = "0.4.1", repos = "http://cran.us.r-project.org")
+"""
