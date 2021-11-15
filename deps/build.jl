5c9cd289a3ede29029d5aaac3119a4a3e0a0df4e
Olivier Labayle <48188914+olivierlabayle@users.noreply.github.com>
2021-11-15 18:13:07 +0100

    Update installation (#5)
    * try change CI procedure
    
    * try add ubuntu and windows
    
    * still not easily working on ubuntu and windows

--- deps/build.jl
+++
@@ -1,7 +0,0 @@
-using RCall
-
-R"""
-install.packages('devtools', repos='http://cran.us.r-project.org')
-require(devtools)
-install_version("hal9001", version = "0.4.1", repos = "http://cran.us.r-project.org")
-"""
